2ebcffd5ada6cf6662a6b424712807e2316a9ab9
ANUJZENDE2456 <anujzende2456@gmail.com>
2026-09-25 11:22:07 +0530

    Add status API

--- test/server.test.js
+++ test/server.test.js
@@ -12,3 +12,9 @@ test('hello API should return Hello World message',async()=>{
     expect(response.status).toBe(200);
     expect(response.body).toEqual({message:"Hello World!"});
 });
+
+test('status API should return running status',async()=>{
+    const response= await request(app).get('/status');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({status:"running"});
+});
